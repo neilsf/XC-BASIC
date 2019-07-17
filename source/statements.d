fc93c7e6b29a149c9fbac3b09aad140ca4b011fb
neils_f <feketecsaba@gmail.com>
2019-07-17 21:19:21 +0200

    Fixed bug in ELSE stmt

--- source/statements.d
+++ source/statements.d
@@ -730,7 +730,9 @@ class Else_stmt:Stmt
 
     void process()
     {
-        this.program.program_segment ~= "_EL_"~ to!string(this.program.if_stack.top()) ~ ":\n";
+        string label_q = to!string(this.program.if_stack.top());
+        this.program.program_segment ~= "\tjmp _EI_" ~label_q~ "\n";
+        this.program.program_segment ~= "_EL_"~ label_q ~ ":\n";
     }
 }
 
