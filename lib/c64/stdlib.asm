99a57090b9f9939facfac1e0f76dcd34df73023f
Majikeyric <Majikeyric@users.noreply.github.com>
2019-11-24 15:21:45 +0100

    Random subroutine CLC fix

--- lib/c64/stdlib.asm
+++ lib/c64/stdlib.asm
@@ -327,10 +327,23 @@ STDLIB_OUTPUT_FLOAT SUBROUTINE
 	jsr KERNAL_PRINTCHR
 	ENDM
 	
-; -------------------------
-; Generate random integer
-; based on the previous one
-; -------------------------
+; ---------------------------------------------------------	
+; Generate a somewhat random repeating sequence.  Uses
+; a typical linear congruential algorithm
+;      I(n+1) = (I(n)*a + c) mod m
+; with m=65536, a=5, and c=13841 ($3611).  c was chosen
+; to be a prime number near (1/2 - 1/6 sqrt(3))*m.
+;
+; Note that in general the higher bits are "more random"
+; than the lower bits, so for instance in this program
+; since only small integers (0..15, 0..39, etc.) are desired,
+; they should be taken from the high byte RANDOM+1, which
+; is returned in A.
+; Taken from Stephen L. Judd's ffd2.com site :
+; http://www.ffd2.com/fridge/math/rand1.s
+; CLC Fix by Kweepa
+; http://sleepingelephant.com/ipw-web/bulletin/bb/viewtopic.php?f=2&t=2304
+; ---------------------------------------------------------	
 	
 STDLIB_RND SUBROUTINE
 	lda random+1
@@ -347,6 +360,7 @@ STDLIB_RND SUBROUTINE
 	adc random+1
 	sta random+1
 	pla
+	clc					; fix
 	adc #$11
 	sta random
 	lda random+1
