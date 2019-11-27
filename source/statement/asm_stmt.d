5d77f7f92ceebadc5514b663854192d34cb9eb2c
neils_f <feketecsaba@gmail.com>
2019-11-27 21:37:20 +0100

    Added sprite extension Inline ASM now supports {self} keyword

--- source/statement/asm_stmt.d
+++ source/statement/asm_stmt.d
@@ -12,8 +12,18 @@ class Asm_stmt:Stmt
     void process()
     {
         string asm_string = stripLeft(chop(join(this.node.children[0].children[0].matches)), "\"");
+
+        if(this.program.in_procedure) {
+            asm_string = this.replace_special_keywords(asm_string);
+        }
+
         this.program.appendProgramSegment("\t; Inline ASM start\n");
         this.program.appendProgramSegment(asm_string~"\n");
         this.program.appendProgramSegment("\t; Inline ASM end\n");
+    }
+
+    private string replace_special_keywords(string asm_string)
+    {
+        return asm_string.replace("{self}", "_" ~ this.program.current_proc_name);
     }
 }
