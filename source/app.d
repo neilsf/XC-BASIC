2e85c80824619c5563c4c1c02fb9a02c8eb00496
neils_f <feketecsaba@gmail.com>
2020-01-23 14:00:32 +0100

    Refactoring exacutable WIP

--- source/app.d
+++ source/app.d
@@ -1,10 +1,11 @@
-import std.stdio, std.file, std.array, std.string, std.getopt, std.path, std.regex;
+import std.stdio, std.file, std.array, std.string, std.getopt, std.path, std.regex, std.random, std.process;
 import core.stdc.stdlib;
 import language.grammar;
 import program;
 import std.conv;
 import globals;
 import optimizer;
+import dini;
 
 int line_count = 0;
 
@@ -13,20 +14,35 @@ int line_count = 0;
  */
 
 bool noopt = false;
+string output_type = "prg";
+string compiler_version = "v2.2.02";
+
 
 void main(string[] args)
 {
+    Ini conf = get_conf();
+
     if(args.length < 2) {
         stderr.writeln("Error: input file not specified");
         exit(1);
     }
 
-     auto helpInformation = getopt(
-        args,
-        "noopt", &noopt,
-     );
+    auto helpInformation = getopt(args,
+        "noopt|n", &noopt,
+        "output|o", &output_type
+    );
+
+    if(helpInformation.helpWanted) {
+        display_help();
+    }
+
+    if(output_type != "prg" && output_type != "asm") {
+        stderr.writeln("Invalid value for option -o");
+        exit(1);
+    }
 
     string filename = args[1];
+    string outname = args[2];
 
     string source = build_source(filename);
     auto ast = XCBASIC(source);
@@ -45,15 +61,54 @@ void main(string[] args)
     program.source_path = absolutePath(dirName(filename));
     program.processAst(ast);
     string code = program.getAsmCode();
-    if(noopt) {
-        writeln(code);
-    }
-    else {
+    if(!noopt) {
         auto optimizer = new Optimizer(code);
         optimizer.run();
-        writeln(optimizer.outcode);
+        code = optimizer.outcode;
     }
 
+    if(output_type == "prg") {
+
+        // Write assembly program to temp location
+        string tmpDir = tempDir();
+        auto rnd = Random(42);
+        auto u = uniform!uint(rnd);
+        string asm_filename = tempDir() ~ "/xcbtmp_" ~ to!string(u, 16) ~ ".asm";
+        File outfile = File(asm_filename, "w");
+        outfile.write(code);
+        outfile.close();
+
+        // Get DASM executable path
+        string path = dirName(thisExePath());
+        string dasm_bin = path ~ dirSeparator ~ conf["assembler"].getKey("dasm_bin");
+
+        // Assemble!
+        auto dasm = executeShell(dasm_bin ~ " " ~ asm_filename ~ " -o" ~ outname);
+        if(dasm.status != 0) {
+            stderr.writeln("There has been an error while trying to execute DASM, please see the bellow message.");
+            stderr.writeln(dasm.output);
+
+            // Remove temp file and exit
+            remove(asm_filename);
+            exit(1);
+        }
+        else {
+
+            // Remove temp file and exit
+            remove(asm_filename);
+            stdout.write(dasm.output);
+            exit(0);
+        }
+
+
+    }
+    else {
+        File outfile = File(outname, "w");
+        outfile.write(code);
+        outfile.close();
+        stdout.writeln("Complete.");
+        exit(0);
+    }
 }
 
 /**
@@ -103,4 +158,43 @@ string build_source(string filename)
     }
 
     return source;
+}
+
+/**
+ * Display help message and exit
+ */
+
+void display_help()
+{
+    stdout.writeln(
+`
+XC=BASIC compiler version ` ~ compiler_version ~ `
+Copyright (c) 2019-2020 by Csaba Fekete
+Usage: xcbasic64 [options] <inputfile> <outputfile> [options]
+Options:
+  --output=<x> or -o<x>  Output type: "prg" (default) or "asm"
+  --noopt or -n          Do not run the optimizer (defaults to false)
+  --help or -h           Show this help
+`
+    );
+    exit(0);
+}
+
+/**
+ * Fetch configuration file
+ */
+
+Ini get_conf()
+{
+    string path = dirName(thisExePath());
+    string filename = path ~ dirSeparator ~ "xcbasic.conf";
+    try {
+        return Ini.Parse(filename);
+    }
+    catch(Exception e) {
+        stderr.writeln("Could not find configuration file in " ~ filename);
+        exit(1);
+    }
+
+    assert(0);
 }
